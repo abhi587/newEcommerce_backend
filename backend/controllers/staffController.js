115c811d441f247333f8878e5706b74a6f4566cf
abhi587 <abhishek.pawan14@gmail.com>
2024-03-22 16:03:40 +0530

    madeChnages

--- backend/controllers/staffController.js
+++ backend/controllers/staffController.js
@@ -1,123 +1,88 @@
-const StaffModel = require("../models/staffModel")
-const catchAsyncErrors = require("../middleware/catchAsyncErrors")
-
+const StaffModel = require("../models/staffModel");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 //************************Add Staff*************************/
 
-
 exports.addStaff = catchAsyncErrors(async (req, res, next) => {
-    const {firstName, lastName, email, mobileNo} = req.body;
-    const staffData = await StaffModel.create(req.body)
+    const { firstName, lastName, email, mobileNo } = req.body;
+    const staffData = await StaffModel.create(req.body);
     res.status(201).send({
-        status: true,
-        message:"new staff is created",
-        data : staffData
-    })
-})
-
-
+        success: true,
+        message: "New staff is created",
+        data: staffData
+    });
+});
 
 //*******************Get All Staffs********************** */
 
 exports.getAllStaffs = catchAsyncErrors(async (req, res, next) => {
-    const staffDetails = await StaffModel.find()
-
-    if(staffDetails.length === 0){
+    const staffDetails = await StaffModel.find();
+    if (staffDetails.length === 0) {
         return res.status(404).send({
-            sucess: false,
+            success: false,
             message: "No data found"
-        })
+        });
     }
-
     return res.status(200).send({
-        sucess: true,
-        message: "data fetched Successfully"
-    })
-})
-
-
+        success: true,
+        message: "Data fetched successfully",
+        data: staffDetails
+    });
+});
 
 //*********************Get Staff Details By id*********** */
 
-exports.getStaffDetailsById = catchAsyncErrors( async ( req, res, next) => {
-    const staffId = req.params.staffId
-
-    const staffdetailsById = await StaffModel.findOne({_id:staffId})
-
-    if(!staffdetailsById){
-        return res.status(400).send({
-            sucess: false,
-            message: "no data found"
-        })
+exports.getStaffDetailsById = catchAsyncErrors(async (req, res, next) => {
+    const staffId = req.params.staffId;
+    const staffdetailsById = await StaffModel.findById(staffId);
+    if (!staffdetailsById) {
+        return res.status(404).send({
+            success: false,
+            message: "No data found"
+        });
     }
-
     return res.status(200).send({
-        sucess: true,
-        message: "data Fetched sucessfully",
+        success: true,
+        message: "Data fetched successfully",
         data: staffdetailsById
-    })
-})
-
-
+    });
+});
 
 //**********************Update Staff Details***************** */
 
-exports.updateStaffDetails = catchAsyncErrors( async (req, res, next) => {
-    
-    const staffId = req.params.staffId
-
-    const {firstName, lastName, email, mobileNo}  = req.body
-
-    const staffDetails = await StaffModel.findOne({_id: staffId})
-
-    if(!staffDetails){
-        return res.status(400).send({
-            sucess: false,
+exports.updateStaffDetails = catchAsyncErrors(async (req, res, next) => {
+    const staffId = req.params.staffId;
+    const { firstName, lastName, email, mobileNo } = req.body;
+    let staffDetails = await StaffModel.findById(staffId);
+    if (!staffDetails) {
+        return res.status(404).send({
+            success: false,
             message: "No data found with this staffId"
-        })
+        });
     }
-
-    if(firstName){
-        staffDetails.firstName = firstName
-    }
-
-    if(lastName){
-        staffDetails.lastName = lastName
-    }
-
-    if(email){
-        staffDetails.email = email
-    }
-
-    if(mobileNo){
-        staffDetails.mobileNo = mobileNo
-    }
-
-    const updateStaffDetails = await staffDetails.save()
-
-    return res.status(201).send({
-        sucess: true,
-        message: "data updated sucessfully",
-        updatedData: updateStaffDetails
-    })
-})
-
-
+    if (firstName) staffDetails.firstName = firstName;
+    if (lastName) staffDetails.lastName = lastName;
+    if (email) staffDetails.email = email;
+    if (mobileNo) staffDetails.mobileNo = mobileNo;
+    staffDetails = await staffDetails.save();
+    return res.status(200).send({
+        success: true,
+        message: "Data updated successfully",
+        updatedData: staffDetails
+    });
+});
 
 //**********************Delete Staff Details****************** */
 
 exports.deleteStaff = catchAsyncErrors(async (req, res, next) => {
-    const staffId = req.params.staffId; 
-    
-    const deletedData = await StaffModel.findOneAndDelete({ _id: staffId });
-    
+    const staffId = req.params.staffId;
+    const deletedData = await StaffModel.findByIdAndDelete(staffId);
     if (!deletedData) {
         return res.status(404).send({
             success: false,
             message: "No data found with this id"
         });
     }
-
     return res.status(200).send({
         success: true,
         message: "Staff deleted successfully"

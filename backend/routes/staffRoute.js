f26959357ae723e5512185a83379e65dfa07a231
abhi587 <abhishek.pawan14@gmail.com>
2024-03-22 15:56:58 +0530

    apiCreated

--- backend/routes/staffRoute.js
+++ backend/routes/staffRoute.js
@@ -1,5 +1,9 @@
 const express = require("express")
-const {addStaff, getAllStaffs} = require("../controllers/staffController")
+const {addStaff, 
+    getAllStaffs, 
+    getStaffDetailsById, 
+    updateStaffDetails, 
+    deleteStaff} = require("../controllers/staffController")
 const {isAuthenticatedUser, authorizeRoles} = require("../middleware/auth");
 const router = express.Router();
 
@@ -14,6 +18,13 @@ router
 router.route("/admin/staff/get")
         .get(getAllStaffs)
 
+router.route("/admin/staff/getById/:staffId")
+        .get(getStaffDetailsById)
 
+router.route("/admin/staff/updateDetails/:staffId")
+        .put(updateStaffDetails)
+
+router.route("/admin/staff/delete/:staffId")
+        .delete(deleteStaff)
         
 module.exports = router;
